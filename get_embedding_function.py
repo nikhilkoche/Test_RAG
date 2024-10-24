69f2daa772c05a9a94454af594eb637bd376426f
Nikhil Koche <nikhilkoche682@gmail.com>
2024-10-24 15:04:54 -0400

    Update get_embedding_function.py

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,7 +1,16 @@
 #from langchain_community.embeddings.ollama import OllamaEmbeddings
 #from langchain_community.embeddings.bedrock import BedrockEmbeddings
 from langchain_ollama import OllamaEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 def get_embedding_function():
-    embeddings = OllamaEmbeddings(model="llama3.1:70b")
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    # embeddings = HuggingFaceEmbeddings(
+    #     model_name="thenlper/gte-base"
+    
+    
+    return embeddings
+
+def get_embedding_function_hug():
+    embeddings = HuggingFaceEmbeddings('thenlper/gte-base')
     return embeddings
